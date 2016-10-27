7c3e2cf7123ee39edcff958d927ea9a9b7105a7a
SanchelliosProg <vasyania2009@gmail.com>
2016-10-27 12:24:02 +0300

    - implemented a qr code scanner

--- app/src/main/java/com/exercizes/sanchellios/bananaqr/CodeListActivity.java
+++ app/src/main/java/com/exercizes/sanchellios/bananaqr/CodeListActivity.java
@@ -1,5 +1,6 @@
 package com.exercizes.sanchellios.bananaqr;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,9 +11,12 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class CodeListActivity extends AppCompatActivity {
 
@@ -24,15 +28,39 @@ public class CodeListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_code_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final Activity activity = this;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CodeListActivity.this, ScannerActivity.class);
-                startActivity(intent);
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setOrientationLocked(false);
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null){
+            if(result.getContents() == null){
+                Log.d("MainActivity", "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            }else {
+                Log.d("MainActivity", "Scanned");
+                Toast.makeText(this, "Scanned: "+result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
